e6de3337131f6d9d7c312ade94d45e200f86756f
Ali Mousazadeh <ali.mousazadeh98@gmail.com>
2020-07-11 20:11:59 +0430

    shortest path implemented

--- routing.py
+++ routing.py
@@ -7,11 +7,49 @@ class Router():
     def __init__(self, subnet, links):
         self.subnet = subnet
         self.links = links
+        self.routingTable = []
 
     def printLinks(self):
         numLinks = len(self.links)
         for i in range(numLinks):
             print(self.links[i][0] + "-" + self.links[i][1])
+
+
+class Graph():
+
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+
+    def minDistance(self, dist, sptSet):
+
+        min = sys.maxsize
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+        return min_index
+
+    def dijkstra(self, src):
+        self.shortestPath = [[] for rows in range(self.V)]
+
+        dist = [sys.maxsize] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+
+        for cout in range(self.V):
+
+            u = self.minDistance(dist, sptSet)
+
+            sptSet[u] = True
+
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and \
+                        sptSet[v] == False and \
+                        dist[v] > dist[u] + self.graph[u][v]:
+                    dist[v] = dist[u] + self.graph[u][v]
+                    self.shortestPath[v].append(u)
 
 
 routerSubnets = []
@@ -44,6 +82,13 @@ for i in range(rows):
         if routers[j].subnet in [link[0] for link in routers[i].links]:
             index = [link[0]
                      for link in routers[i].links].index(routers[j].subnet)
-            distanceGraph[i][j] = routers[i].links[index][1]
+            distanceGraph[i][j] = int(routers[i].links[index][1])
         else:
             distanceGraph[i][j] = 0
+
+
+g = Graph(len(routers))
+g.graph = distanceGraph
+
+g.dijkstra(0)
+print(g.shortestPath)
